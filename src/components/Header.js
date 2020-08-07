fa09661df6e00e3d43303fe328135cf26424177d
PetrovIlyuha <dnamix1@gmail.com>
2020-08-07 13:05:27 +0300

    header ready excluding media-queries support

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { withRouter, NavLink } from "react-router-dom";
+import Logo from "./Branding/Logo";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="flex">
+        <Logo className="header-logo" />
+        <NavLink to="/" className="header-title">
+          Tech News
+        </NavLink>
+        <NavLink to="/" className="header-link">
+          New
+        </NavLink>
+        <div className="divider">|</div>
+        <NavLink to="/top" className="header-link">
+          Top Rated
+        </NavLink>
+        <div className="divider">|</div>
+        <NavLink to="/search" className="header-link">
+          Search
+        </NavLink>
+        <div className="divider">|</div>
+        <NavLink to="/create" className="header-link">
+          Submit
+        </NavLink>
+      </div>
+      <div className="flex">
+        <NavLink to="/login" className="header-link-login">
+          Login
+        </NavLink>
+      </div>
+    </div>
+  );
 }
 
-export default Header;
+export default withRouter(Header);
