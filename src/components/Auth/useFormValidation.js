9ed11b82f31375f5098cb94cd6f491afb6039499
PetrovIlyuha <dnamix1@gmail.com>
2020-08-08 10:42:57 +0300

    singup form validation custom hook

--- src/components/Auth/useFormValidation.js
+++ src/components/Auth/useFormValidation.js
@@ -1,5 +1,49 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
-function useFormValidation() {}
+function useFormValidation(initialState, validate, authenticateUser) {
+  const [values, setValues] = useState(initialState);
+  const [errors, setErrors] = useState({});
+  const [isSubmitting, setSubmitting] = useState(false);
+
+  useEffect(() => {
+    if (isSubmitting) {
+      const errorsCleared = Object.keys(errors).length === 0;
+      if (errorsCleared) {
+        setSubmitting(false);
+        authenticateUser();
+      } else {
+        setSubmitting(false);
+      }
+    }
+  }, [errors]);
+
+  const handleChange = (e) => {
+    e.persist();
+    setValues((prevValues) => ({
+      ...prevValues,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleBlur = () => {
+    const validationErrors = validate(values);
+    setErrors(validationErrors);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const validationErrors = validate(values);
+    setErrors(validationErrors);
+    setSubmitting(true);
+  };
+  return {
+    handleChange,
+    handleSubmit,
+    handleBlur,
+    isSubmitting,
+    values,
+    errors,
+  };
+}
 
 export default useFormValidation;
