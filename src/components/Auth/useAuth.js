e1dc34fe722107ef17f2d96feca482ae35236d09
PetrovIlyuha <dnamix1@gmail.com>
2020-08-08 10:45:21 +0300

    use authentication custom react hook

--- src/components/Auth/useAuth.js
+++ src/components/Auth/useAuth.js
@@ -1,5 +1,19 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import firebase from "../../firebase";
 
-function useAuth() {}
+function useAuth() {
+  const [authUser, setAuthUser] = useState(null);
+  const unsubscribe = useEffect(() => {
+    firebase.auth.onAuthStateChanged((user) => {
+      if (user) {
+        setAuthUser(user);
+      } else {
+        setAuthUser(null);
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+  return authUser;
+}
 
 export default useAuth;
