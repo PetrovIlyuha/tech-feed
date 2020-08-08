6d1e2332fd0744265707c6900986bb2e3073601a
PetrovIlyuha <dnamix1@gmail.com>
2020-08-08 10:47:08 +0300

    authcontext wrapping the main entry point App.js providing the values of user after authentication and the firebase class to perform auth ops inside components down the tree

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import useAuth from "./Auth/useAuth";
 import {
   BrowserRouter as Router,
   Switch,
@@ -12,25 +13,30 @@ import ForgotPassword from "./Auth/ForgotPassword";
 import SearchLinks from "./Link/SearchLinks";
 import LinkList from "./Link/LinkList";
 import LinkDetail from "./Link/LinkDetail";
+import firebase, { AuthContext } from "../firebase";
 
 function App() {
+  const user = useAuth();
+
   return (
     <Router>
-      <div className="app-container">
-        <Header />
-        <div className="route-container">
-          <Switch>
-            <Route exact path="/" render={() => <Redirect to="/new/1" />} />
-            <Route path="/create" component={CreateLink} />
-            <Route path="/login" component={Login} />
-            <Route path="/forgot" component={ForgotPassword} />
-            <Route path="/search" component={SearchLinks} />
-            <Route path="/top" component={LinkList} />
-            <Route path="/new/:page" component={LinkList} />
-            <Route path="/link/:linkId" component={LinkDetail} />
-          </Switch>
+      <AuthContext.Provider value={{ user, firebase }}>
+        <div className="app-container">
+          <Header />
+          <div className="route-container">
+            <Switch>
+              <Route exact path="/" render={() => <Redirect to="/new/1" />} />
+              <Route path="/create" component={CreateLink} />
+              <Route path="/login" component={Login} />
+              <Route path="/forgot" component={ForgotPassword} />
+              <Route path="/search" component={SearchLinks} />
+              <Route path="/top" component={LinkList} />
+              <Route path="/new/:page" component={LinkList} />
+              <Route path="/link/:linkId" component={LinkDetail} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </AuthContext.Provider>
     </Router>
   );
 }
