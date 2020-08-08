9a3ec3d3ca1982b266f24b0762b8d21833fa8075
PetrovIlyuha <dnamix1@gmail.com>
2020-08-08 10:47:46 +0300

    reset password page component

--- src/components/Auth/ForgotPassword.js
+++ src/components/Auth/ForgotPassword.js
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { AuthContext } from "../../firebase";
 
 function ForgotPassword() {
-  return <div>ForgotPassword</div>;
+  const { firebase } = useContext(AuthContext);
+  const [resetPasswordEmail, setResetPasswordEmail] = useState("");
+  const [isPasswordResetSuccess, setIsPasswordResetSuccess] = useState(false);
+  const [passResetErr, setPassResetErr] = useState(null);
+  const handleResetPassword = async () => {
+    try {
+      await firebase.resetPassword(resetPasswordEmail);
+      setIsPasswordResetSuccess(true);
+      setPassResetErr(null);
+    } catch (err) {
+      setPassResetErr(err);
+      setIsPasswordResetSuccess(false);
+      console.error("Error sending email", err);
+    }
+  };
+  return (
+    <div>
+      <input
+        style={{ marginTop: 20 }}
+        type="email"
+        className="login-input"
+        placeholder="Where to send reset link?"
+        value={resetPasswordEmail}
+        onChange={(e) => setResetPasswordEmail(e.target.value)}
+      />
+      <div>
+        <button
+          style={{ marginTop: 20 }}
+          className="button login-button"
+          onClick={handleResetPassword}
+        >
+          Reset Password
+        </button>
+      </div>
+      {isPasswordResetSuccess && (
+        <p>Check your inbox for the link to reset password!</p>
+      )}
+      {passResetErr && <p className="error-text">{passResetErr.message}</p>}
+    </div>
+  );
 }
 
 export default ForgotPassword;
