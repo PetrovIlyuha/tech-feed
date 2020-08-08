af3e645969776313d5ecc326e0707e5c4d706d89
PetrovIlyuha <dnamix1@gmail.com>
2020-08-08 10:43:44 +0300

    validation form input fields utility function

--- src/components/Auth/validateLogin.js
+++ src/components/Auth/validateLogin.js
@@ -1,1 +1,15 @@
-export default function validateLogin(values) {}
+export default function validateLogin(values) {
+  let errors = {};
+  let emailRegexp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/gim;
+  if (!values.email) {
+    errors.email = "Email required";
+  } else if (!emailRegexp.test(values.email)) {
+    errors.email = "Please provide valid email";
+  }
+  if (!values.password) {
+    errors.password = "Password required";
+  } else if (values.password.length < 6) {
+    errors.password = "Password too short";
+  }
+  return errors;
+}
