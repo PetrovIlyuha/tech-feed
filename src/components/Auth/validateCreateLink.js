fb0e40a06b0ee28138c50ddfd9213a4ae975784e
PetrovIlyuha <dnamix1@gmail.com>
2020-08-09 11:47:52 +0300

    link creation validation function

--- src/components/Auth/validateCreateLink.js
+++ src/components/Auth/validateCreateLink.js
@@ -1,1 +1,14 @@
-export default function validateCreateLink(values) {}
+export default function validateCreateLink(values) {
+  let errors = {};
+  if (!values.description) {
+    errors.description = "Description required";
+  } else if (values.description.length < 10) {
+    errors.description = "Description too short.. 10 or more characters";
+  }
+  if (!values.url) {
+    errors.url = "URL is needed";
+  } else if (!/^(ftp|http|https):\/\/[^ "]+$/.test(values.url)) {
+    errors.url = "given URL is not valid";
+  }
+  return errors;
+}
