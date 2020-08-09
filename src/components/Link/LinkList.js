944cb4a093ae47c1ad0547d58701e513116cc340
PetrovIlyuha <dnamix1@gmail.com>
2020-08-09 11:49:23 +0300

    links list implemented (firebase retrieval of all links and returning mapped array of these in JSX

--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import FirebaseContext from "../../firebase/context";
+
+import LinkItem from "./LinkItem";
 
 function LinkList(props) {
-  return <div>LinkList</div>;
+  const { firebase } = useContext(FirebaseContext);
+  const [links, setLinks] = useState([]);
+
+  useEffect(() => {
+    getLinks();
+  }, []);
+
+  function getLinks() {
+    firebase.db.collection("links").onSnapshot(handleSnapshot);
+  }
+
+  function handleSnapshot(snapshot) {
+    const links = snapshot.docs.map((doc) => {
+      return { id: doc.id, ...doc.data() };
+    });
+    setLinks(links);
+  }
+
+  return (
+    <div>
+      {links.map((link, idx) => (
+        <LinkItem key={idx} link={link} showCount={true} index={idx + 1} />
+      ))}
+    </div>
+  );
 }
 
 export default LinkList;
