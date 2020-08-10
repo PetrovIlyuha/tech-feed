0859de9bbf02cc8a000b16d4c6538970b18b7c35
PetrovIlyuha <dnamix1@gmail.com>
2020-08-10 14:20:12 +0300

    link list with pagination

--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -3,30 +3,88 @@ import FirebaseContext from "../../firebase/context";
 
 import LinkItem from "./LinkItem";
 
+const LINKS_PER_PAGE = 5;
+
 function LinkList(props) {
   const { firebase } = useContext(FirebaseContext);
   const [links, setLinks] = useState([]);
+  const [cursor, setCursor] = useState(null);
+  const isTopPage = props.location.pathname.includes("top");
+  const isNewPage = props.location.pathname.includes("new");
+  const page = +props.match.params.page;
 
   useEffect(() => {
-    getLinks();
-  }, []);
+    const unsubscribe = getLinks();
+    return () => unsubscribe();
+  }, [isTopPage, page]);
 
   function getLinks() {
-    firebase.db.collection("links").onSnapshot(handleSnapshot);
+    const hasCursor = !!cursor;
+    if (isTopPage) {
+      return firebase.db
+        .collection("links")
+        .orderBy("voteCount", "desc")
+        .limit(LINKS_PER_PAGE)
+        .onSnapshot(handleSnapshot);
+    } else if (page === 1) {
+      return firebase.db
+        .collection("links")
+        .orderBy("created", "desc")
+        .limit(LINKS_PER_PAGE)
+        .onSnapshot(handleSnapshot);
+    } else if (hasCursor) {
+      return firebase.db
+        .collection("links")
+        .orderBy("created", "desc")
+        .startAfter(cursor.created)
+        .limit(LINKS_PER_PAGE)
+        .onSnapshot(handleSnapshot);
+    }
   }
 
   function handleSnapshot(snapshot) {
     const links = snapshot.docs.map((doc) => {
       return { id: doc.id, ...doc.data() };
     });
+    const lastLink = links[links.length - 1];
     setLinks(links);
+    setCursor(lastLink);
   }
+
+  const visitPrevPage = () => {
+    if (page > 1) {
+      props.history.push(`/new/${page - 1}`);
+    }
+  };
+
+  const visitNextPage = () => {
+    if (page <= links.length / LINKS_PER_PAGE) {
+      props.history.push(`/new/${page + 1}`);
+    }
+  };
+
+  const pageIndex = page ? (page - 1) * LINKS_PER_PAGE + 1 : 0;
 
   return (
     <div>
       {links.map((link, idx) => (
-        <LinkItem key={idx} link={link} showCount={true} index={idx + 1} />
+        <LinkItem
+          key={idx}
+          link={link}
+          showCount={true}
+          index={idx + pageIndex}
+        />
       ))}
+      {isNewPage && (
+        <div className="pagination">
+          <div className="pointer mr2" onClick={visitPrevPage}>
+            Prev
+          </div>
+          <div className="pointer" onClick={visitNextPage}>
+            Next
+          </div>
+        </div>
+      )}
     </div>
   );
 }
